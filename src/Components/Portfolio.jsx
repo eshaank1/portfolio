51f4f65370673911600fa0d4ba355568460f9e51
Eshaan Kumar <59542863+eshaank1@users.noreply.github.com>
2024-04-13 01:34:18 +0000

    projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "EcoSort",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Mobile app to identify waste as trash, recycle, or compost.",
+    url: "https://github.com/eshaank1/ecosort/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Chat",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Online chatroom app.",
+    url: "https://eshaank1.github.io/chat/",
   },
   {
-    title: "My Resume Site",
+    title: "StockSense",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Stock and crypto analysis and portfolio management tool.",
+    url: "https://eshaank1.github.io/stocksense/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "CSP Blog",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Blog of my journey through the AP Computer Science Principles course, including some projects I developed along the way.",
+    url: "https://eshaank1.github.io/csp/",
   },
 ];
 
