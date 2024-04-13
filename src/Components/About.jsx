e58c5b5313dc93cfd774f6f2fe9d0abb52950dd4
Eshaan Kumar <59542863+eshaank1@users.noreply.github.com>
2024-04-13 00:10:38 +0000

    about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a student studying at Del Norte High School. I enjoy programming, learning new things, and teaching others.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Database management",
+  "Product design",
+  "Testing",
+  "Mobile app development",
+  "Cybersecurity",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my classroom learning I continually look for new and better ways to make tech useful for all.";
 
 const About = () => {
   return (
